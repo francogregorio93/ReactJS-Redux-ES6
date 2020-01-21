40b957f24995e3b64e0e94e64f05c5660aecb20d
Franco Gregorio <francogregorio@MacBook-Air-de-Franco.local>
2020-01-20 21:51:19 -0300

    first component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import WheaterLocation from './components/WeatherLocation';
+function App() {
+  return (
+    <div className="App">
+    hola mundo
+    <WheaterLocation></WheaterLocation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const WheaterLocation = ()=>(
+    <div>Wheater Location</div>
+);
+
+export default WheaterLocation;
